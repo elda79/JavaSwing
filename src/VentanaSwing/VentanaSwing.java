f4426fda0e478311c831f9d7b55cc5fbc00dc434
negrita79 <elisa.79@live.cl>
2017-11-08 20:39:42 -0300

    Importamos todo de swing

---
+++ src/VentanaSwing/VentanaSwing.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VentanaSwing;
+import javax.swing.*;//Importamos todo de swing
+import java.awt.*;
+/**
+ *
+ * @author LAB05
+ */
+public class VentanaSwing {
+    
+}
